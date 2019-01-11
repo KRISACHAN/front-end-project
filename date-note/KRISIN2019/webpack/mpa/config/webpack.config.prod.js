a4c185469799bbd0946a253188844af17ebf7f7f
krisdw <chenjinwen77@foxmail.com>
2019-01-11 11:15:36 +0800

    webpack demo optimized

--- date-note/KRISIN2019/webpack/mpa/config/webpack.config.prod.js
+++ date-note/KRISIN2019/webpack/mpa/config/webpack.config.prod.js
@@ -3,23 +3,24 @@ const webpack = require('webpack')
 const webpackMerge = require('webpack-merge')
 const cleanWebpackPlugin = require('clean-webpack-plugin')
 const uglifyJSPlugin = require('uglifyjs-webpack-plugin')
-const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin')
+// const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const webpackBase = require('./webpack.config.base.js')
 const config = require('./config.js')
 
-const cssExtracter = new ExtractTextWebpackPlugin({
-  filename: './css/[name]-css.[contenthash:8].css',
-  allChunks: true
-})
-const sassExtracter = new ExtractTextWebpackPlugin({
-  filename: './css/[name]-sass.[contenthash:8].css',
-  allChunks: true
-})
-const lessExtracter = new ExtractTextWebpackPlugin({
-  filename: './css/[name]-less.[contenthash:8].css',
-  allChunks: true
-})
+// const cssExtracter = new ExtractTextWebpackPlugin({
+//   filename: './css/[name]-css.[contenthash:8].css',
+//   allChunks: true
+// })
+// const sassExtracter = new ExtractTextWebpackPlugin({
+//   filename: './css/[name]-sass.[contenthash:8].css',
+//   allChunks: true
+// })
+// const lessExtracter = new ExtractTextWebpackPlugin({
+//   filename: './css/[name]-less.[contenthash:8].css',
+//   allChunks: true
+// })
 
 const webpackProd = {
   mode: 'production',
@@ -29,55 +30,83 @@ const webpackProd = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        include: [config.src],
-        exclude: [config.vendors],
-        use: cssExtracter.extract({
-          fallback: 'style-loader',
-          use: [{
-              loader: 'css-loader',
-              options: {
-                minimize: true
-              }
-          }, 'postcss-loader'],
-          publicPath: '../'
-        })
-      },
-      {
-        test: /\.scss$/,
-        include: [config.src],
-        exclude: [config.vendors],
-        use: sassExtracter.extract({
-          fallback: 'style-loader',
-          use: [{
-              loader: 'css-loader',
-              options: {
-                  minimize: true
-              }
-          }, 'postcss-loader', 'sass-loader'],
-          publicPath: '../'
-        })
-      },
-      {
-        test: /\.less$/,
-        include: [config.src],
-        exclude: [config.vendors],
-        use: lessExtracter.extract({
-          fallback: 'style-loader',
-          use: [{
-              loader: 'css-loader',
-              options: {
-                  minimize: true
-              }
-          }, 'postcss-loader', 'less-loader'],
-          publicPath: '../'
-        })
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
       }
+//       {
+//         test: /\.css$/,
+//         include: [config.src],
+//         exclude: [config.vendors],
+//         use: ExtractTextWebpackPlugin.extract({
+//           fallback: {
+//             loader: 'style-loader',
+//               options: {
+//                 singleton: true
+//               }
+//           },
+//           use: [{
+//               loader: 'css-loader',
+//               options: {
+//                 minimize: true
+//               }
+//           }, 'postcss-loader'],
+//           publicPath: '../'
+//         })
+//       },
+//       {
+//         test: /\.scss$/,
+//         include: [config.src],
+//         exclude: [config.vendors],
+//         use: ExtractTextWebpackPlugin.extract({
+//           fallback: {
+//             loader: 'style-loader',
+//               options: {
+//                 singleton: true
+//               }
+//           },
+//           use: [{
+//               loader: 'css-loader',
+//               options: {
+//                 minimize: true
+//               }
+//           }, 'postcss-loader', 'sass-loader'],
+//           publicPath: '../'
+//         })
+//       },
+//       {
+//         test: /\.less$/,
+//         include: [config.src],
+//         exclude: [config.vendors],
+//         use: ExtractTextWebpackPlugin.extract({
+//           fallback: {
+//             loader: 'style-loader',
+//               options: {
+//                 singleton: true
+//               }
+//           },
+//           use: [{
+//               loader: 'css-loader',
+//               options: {
+//                 minimize: true
+//               }
+//           }, 'postcss-loader', 'less-loader'],
+//           publicPath: '../'
+//         })
     ]
   },
   plugins: [
-    cssExtracter,
-    sassExtracter,
+//     cssExtracter,
+//     sassExtracter,
+//     lessExtracter,
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[hash].css',
+      chunkFilename: 'css/[id].[hash].css'
+    }),
     new webpack.HashedModuleIdsPlugin(),
     new cleanWebpackPlugin(['./dist/'], {
       root: config.project
