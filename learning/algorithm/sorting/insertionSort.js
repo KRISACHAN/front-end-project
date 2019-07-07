46fce023487110906d5b81b6c9b25c13c92aa652
krisdw <chenjinwen77@foxmail.com>
2019-07-07 22:46:31 +0800

    yingyingying

--- learning/algorithm/sorting/insertionSort.js
+++ learning/algorithm/sorting/insertionSort.js
@@ -4,12 +4,13 @@ const insertionSort = arr => {
     const len = arr.length
     let j, temp
     for (let i = 0; i < len; ++i) {
-        j = i /* 存储当前索引，便于后续与数组其他元素对比 */
-        while (j > 0 && arr[j - 1] > temp) {
-            arr[j] = arr[j - 1]
-            j--
+        j = i - 1
+        temp = arr[i]
+        while (j >= 0 && arr[j] > temp) {
+          arr[j + 1] = arr[j]
+          j--
         }
-        [arr[j], arr[i]] = [arr[i], arr[i]]
+        arr[j + 1] = temp
     }
     return arr
 }
