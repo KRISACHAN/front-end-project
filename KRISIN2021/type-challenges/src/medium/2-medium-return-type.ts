3101bd0c3720a3eed81b5c6bf429c2b70231d6a4
krisachan <chenjinwen77@foxmail.com>
2021-08-05 09:31:37 +0800

    feat: 添加题目

---
+++ KRISIN2021/type-challenges/src/medium/2-medium-return-type.ts
@@ -0,0 +1,23 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type MyReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : any
+
+type ComplexObject = {
+    a: [12, 'foo']
+    bar: 'hello'
+    prev(): number
+  }
+  
+const fn = (v: boolean) => v ? 1 : 2
+const fn1 = (v: boolean, w: any) => v ? 1 : 2
+
+type cases = [
+    Expect<Equal<string, MyReturnType<() => string>>>,
+    Expect<Equal<ReturnType<() => string>, MyReturnType<() => string>>>,
+    Expect<Equal<123, MyReturnType<() => 123>>>,
+    Expect<Equal<ComplexObject, MyReturnType<() => ComplexObject>>>,
+    Expect<Equal<Promise<boolean>, MyReturnType<() => Promise<boolean>>>>,
+    Expect<Equal<() => 'foo', MyReturnType<() => () => 'foo'>>>,
+    Expect<Equal<1 | 2, MyReturnType<typeof fn>>>,
+    Expect<Equal<1 | 2, MyReturnType<typeof fn1>>>,
+]
---
+++ KRISIN2021/type-challenges/src/medium/3-medium-omit.ts
@@ -0,0 +1,23 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type MyOmit<T, K extends string | number | symbol> = { [P in Exclude<keyof T, K>]: T[P]; }
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+interface Expected1 {
+  title: string
+  completed: boolean
+}
+
+interface Expected2 {
+  title: string
+}
+
+type cases = [
+    Expect<Equal<Expected1, MyOmit<Todo, 'description'>>>,
+    Expect<Equal<Expected2, MyOmit<Todo, 'description' | 'completed'>>>
+]
