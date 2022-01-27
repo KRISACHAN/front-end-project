85db322ee57d930e6e4cda25e6a1d83aa07e4b16
krisachan <chenjinwen77@foxmail.com>
2022-01-27 15:53:42 +0800

    fix: ..

--- KRISIN2021/type-challenges/src/test.ts
+++
@@ -1,8 +0,0 @@
-enum TargetTypeEnum {
-    ios = '苹果',
-    android = '安卓',
-    all = '所有用户',
-    user_ids = '指定用户',
-}
-
-type TargetType = keyof typeof TargetTypeEnum;
--- KRISIN2021/type-challenges/src/warm-up/13-warm-hello-world.ts
+++
@@ -1,8 +0,0 @@
-import { Equal, Expect, NotAny } from '@type-challenges/utils'
-
-type HelloWorld = string
-
-type cases = [
-    Expect<NotAny<HelloWorld>>,
-    Expect<Equal<HelloWorld, string>>
-]
