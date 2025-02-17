d8463963ca68c6532ceca3d68616a3773abaa0e3
fish_head <chenjinwen77@foxmail.com>
2025-02-18 02:07:29 +0800

    用英语重刷代码随想录

--- KRISIN2025/programmercarl/1.array/1.704.二分查找/自己写的.js
+++ KRISIN2025/programmercarl/1.array/1.704.二分查找/自己写的.js
@@ -1,42 +1,42 @@
-// https://leetcode.cn/problems/binary-search/description/
+// https://leetcode.com/problems/binary-search/description/
 
 /**
- * 二分查找算法
- * 在一个有序数组中查找目标值，返回目标值的下标，如果不存在返回-1
- * 时间复杂度：O(log n)
- * 空间复杂度：O(1)
+ * binary / ˈbaɪnəri / search
+ * runtime complexity / kəmˈpleksəti /：O(log n)
+ * space complexity：O(1)
  *
- * @param {number[]} nums - 升序排列的整数数组
- * @param {number} target - 要查找的目标值
- * @return {number} - 目标值在数组中的下标，不存在则返回-1
+ * @param {number[]} nums - sorted array in ascending order
+ * @param {number} target - value to search for
+ * @return {number} - index of target if found, -1 otherwise
  */
 var search = function (nums, target) {
-    // 初始化左右指针
-    let left = 0;
-    let right = nums.length - 1;
-    let mid = 0;
+    // set the left, right boundaries
+    let left = 0, right = nums.length - 1;
 
-    // 当左指针小于等于右指针时继续查找
+    // use while loop to narrow the range of the result
     while (left <= right) {
-        // 计算中间位置
-        // (right - left) >> 1 等价于 Math.floor((right - left) / 2)
-        // >> 1 表示向右移位1位，相当于除以2并向下取整，比除法运算更高效
-        mid = left + ((right - left) >> 1);
-
-        // 如果中间值大于目标值，在左半部分继续查找
+        // ensure the pivot / ˈpɪvət / point
+        let mid = left + ((right - left) >> 1);
+        // if target is bigger that nums[mid]
+        // it means the result is in the right side of nums
         if (nums[mid] > target) {
             right = mid - 1;
-        }
-        // 如果中间值小于目标值，在右半部分继续查找
-        else if (nums[mid] < target) {
+        // if target is smaller that nums[mid]
+        // it means the result is in the left side of nums
+        } else if (nums[mid] < target) {
             left = mid + 1;
-        }
-        // 找到目标值，返回其下标
-        else {
+        } else {
+            // if not the above two cases
+            // it means mid is the result
             return mid;
         }
     }
 
-    // 未找到目标值，返回-1
+    // if target isn't in nums, return -1
     return -1;
 };
+
+var nums = [-1,0,3,5,9,12];
+
+console.log(search(nums, 9)) // 4
+console.log(search(nums, 2)) // -1
