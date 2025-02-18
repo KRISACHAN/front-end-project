3f8ca76818ef3f11480acaa97c1bb2254b2cf74d
fish_head <chenjinwen77@foxmail.com>
2025-02-19 01:22:28 +0800

    添加题目

--- KRISIN2025/programmercarl/1.array/2.27.移除元素/自己写的.js
+++ KRISIN2025/programmercarl/1.array/2.27.移除元素/自己写的.js
@@ -1,30 +1,28 @@
-// https://leetcode.cn/problems/remove-element/description/
+// https://leetcode.com/problems/remove-element/description/
 
 /**
- * @param {number[]} nums 输入数组
- * @param {number} val 需要移除的目标值
- * @return {number} 返回移除元素后数组的新长度
- *
- * 时间复杂度：O(n)，其中 n 是数组的长度。需要遍历一次数组。
- * 空间复杂度：O(1)，只使用了常数个变量，没有使用额外的数组空间。
- * 算法采用双指针（快慢指针）思想，extraIndex 作为慢指针，index 作为快指针。
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
  */
-var removeElement = function (nums, val) {
-    // extraIndex 用于记录新数组的位置，同时也代表新数组的长度
-    let extraIndex = 0;
-
-    // 遍历原数组
-    for (let index = 0; index < nums.length; ++index) {
-        // 当前元素不等于目标值时
-        if (nums[index] !== val) {
-            // 将当前元素移动到新数组的位置
-            nums[extraIndex] = nums[index];
-            // 新数组位置后移
-            extraIndex += 1;
-        }
-        // 如果当前元素等于val，则跳过，不做任何操作
-    }
-
-    // 返回新数组的长度
-    return extraIndex;
+var removeElement = function(nums, val) {
+    // set a slow pointer to update the pre-replaced location
+    let slowPointer = 0;
+    // use for loop to represent a fast pointer routing
+    for (let fastPointer = 0, len = nums.length; fastPointer < len; ++fastPointer) {
+        // the main thinking is moving fastPointer step by step to get current target
+        // and if we find the matched target is not equal to val
+        // we can move slowPointer at the same time
+        // on the contrary, if  the matched target is equal to val
+        // we only need to stop moving slowPointer
+        // if so, in next step, we can remove the equalled target
+        // and make the next value moved forward
+        if (nums[fastPointer] !== val) {
+            nums[slowPointer++] = nums[fastPointer];
+        };
+    };
+    return slowPointer;
 };
+
+console.log(removeElement([3,2,2,3], 2)) // 2
+console.log(removeElement([0,1,2,2,3,0,4,2], 2)) // 5
